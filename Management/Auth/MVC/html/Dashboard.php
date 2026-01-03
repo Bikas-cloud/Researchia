3c163b0d4851812dd609e3dca8592cbd1d0ebcc6
Bikas-cloud <shilb677@gmail.com>
2026-01-04 05:13:52 +0600

    dashboard styling

--- Management/Auth/MVC/html/Dashboard.php
+++ Management/Auth/MVC/html/Dashboard.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $site_name; ?> - Welcome</title>
     
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/dashboardStyle.css">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
