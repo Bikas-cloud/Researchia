ce7e2f2922aba8b29b810e23dc0e6f3785305556
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-17 05:32:55 +0600

    firstpaperwork

---
+++ Management/Author/MVC/php/myPapers.php
@@ -0,0 +1,18 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Papers</title>
+    <link rel="stylesheet" href="../css/myPapers.css">
+</head>
+<body>
+
+
+
+</body>
+</html>
