c27cc34eb1779bf53d751fef89f08025316d3772
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-17 04:56:24 +0600

    seconddeshboard

--- Management/Author/MVC/php/authorDashboard.php
+++ Management/Author/MVC/php/authorDashboard.php
@@ -6,6 +6,16 @@
     <link rel="stylesheet" href="../css/authorDashboard.css">
 </head>
 <body>
+<div class="dashboard">
+    <h1>Author Dashboard</h1>
+    <div class="cards">
+        <a href="addPaper.php" class="card">Submit Papers</a>
+        <a href="mypapers.php" class="card">My Papers</a>
+        <a href="submitpaper.php" class="card">Reviewer Feedback</a>
+        <a href="../../../Auth/MVC/php/profile.php" class="card">Profile</a>        
+        
+        <a href="/Research_Project/Management/Auth/MVC/php/logout.php" class="card logout">Logout</a>
+    </div>
 
 
 </body>
