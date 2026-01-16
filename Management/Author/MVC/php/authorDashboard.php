3283aa588993dd24cac0528da558dd3f45a51530
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-17 04:54:14 +0600

    dashboard start

---
+++ Management/Author/MVC/php/authorDashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Author Dashboard</title>
+    <link rel="stylesheet" href="../css/authorDashboard.css">
+</head>
+<body>
+
+
+</body>
