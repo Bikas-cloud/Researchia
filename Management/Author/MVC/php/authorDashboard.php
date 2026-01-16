73271b2d3e9d828adc6dc38e27cf6c010b87c9ef
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-17 05:19:07 +0600

    sixthdashboard

--- Management/Author/MVC/php/authorDashboard.php
+++ Management/Author/MVC/php/authorDashboard.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+require_once "../../../Auth/MVC/db/db.php";
+
+// Check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header("Location: /Research_Project/Management/Auth/MVC/php/login.php");
+    exit;
+}
+
+// Basic author protection
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'author') {
+    header("Location: /Research_Project/Management/Auth/MVC/php/login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
