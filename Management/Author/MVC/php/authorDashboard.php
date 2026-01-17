88fc16d6ea9790aca22e8d4e2dfd9cec57e8954e
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-17 06:26:52 +0600

    1stwork

--- Management/Author/MVC/php/authorDashboard.php
+++ Management/Author/MVC/php/authorDashboard.php
@@ -6,3 +6,19 @@
     <title>Author Dashboard</title>
     <link rel="stylesheet" href="../css/authorDashboard.css">
 </head>
+<body>
+<div class="dashboard">
+    <h1>Author Dashboard</h1>
+    <div class="cards">
+        <a href="addPaper.php" class="card">Submit Papers</a>
+        <a href="mypapers.php" class="card">My Papers</a>
+        <a href="submitpaper.php" class="card">Reviewer Feedback</a>
+        <a href="../../../Auth/MVC/php/profile.php" class="card">Profile</a>        
+        <a href="/Research_Project/Management/Auth/MVC/php/logout.php" class="card logout">Logout</a>
+    </div>
+    
+</div>
+
+<script src="../js/adminDashboard.js"></script>
+</body>
+</html>
