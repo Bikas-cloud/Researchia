8a21a84437b077b35a935a59a89b35445eb962f8
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-19 20:11:11 +0600

    1st review

--- Management/Reviewer/MVC/php/reviewerDashboard.php
+++ Management/Reviewer/MVC/php/reviewerDashboard.php
@@ -1,24 +1,19 @@
-<?php
-session_start();
-require_once "../../../Auth/MVC/db/db.php"; 
-/* Reviewer protection */
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'reviewer') {
-    header("Location: /Research_Project/Management/Auth/MVC/php/index.php");
-    exit;
-}
 
 
-?>
-<html>
-    <head>
-        <title>
-            Author Dashboard
-        </title>
-    </head>
-    <body>
-        <h1>Welcome to Author Dashboard</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reviewer Dashboard</title>
+    <link rel="stylesheet" href="../css/reviewerDashboard.css">
+</head>
+<body>
 
-        
-         <a href="../../../Auth/MVC/php/Profile.php">View Profile</a>
-    </body>
+
+
+    <h1>Reviewer Dashboard</h1>
+
+    
+
+</body>
 </html>
