74ab83951907282f60f7da02b4927519b9baadd8
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-19 22:00:52 +0600

    reviewer feedback.php 1st

---
+++ Management/Author/MVC/php/submitpaper.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reviewer Feedback</title>
+    <link rel="stylesheet" href="../css/submitpaper.css">
+</head>
+<body>
+    <h2>Reviewer Feedback</h2>
+
+    
+</body>
+</html>
