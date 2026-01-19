5789e7942314b76857d6e1c4d018e454820040e7
Bikas-cloud <shilb677@gmail.com>
2026-01-19 22:03:19 +0600

    change the path

--- Management/Auth/MVC/php/index.php
+++ Management/Auth/MVC/php/index.php
@@ -2,9 +2,9 @@
 require_once "../db/db.php";
 
 if (isset($_SESSION['user_id'])) {
-    header("Location: authorDashboard.php");
-} else {
     header("Location: login.php");
+} else {
+    header("Location: ../html/Dashboard.php");
 }
 exit();
 ?>
