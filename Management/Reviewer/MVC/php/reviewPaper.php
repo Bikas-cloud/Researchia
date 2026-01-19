d3ad7e12444355a60918c1d8d504f65b933d513b
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-19 20:30:05 +0600

    1stpaper

---
+++ Management/Reviewer/MVC/php/reviewPaper.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Review Paper</title>
+    <link rel="stylesheet" href="../css/reviewPaper.css">
+</head>
+<body>
+    <h2>Review Paper</h2>
+</body>
+</html>
