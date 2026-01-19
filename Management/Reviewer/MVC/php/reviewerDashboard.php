6e15903098c018665b153fd1a204795a64474b35
Aurnobpaul <22-48859-3@student.aiub.edu>
2026-01-19 20:17:23 +0600

    3rd

--- Management/Reviewer/MVC/php/reviewerDashboard.php
+++ Management/Reviewer/MVC/php/reviewerDashboard.php
@@ -20,7 +20,22 @@
         <a href="/Research_Project/Management/Auth/MVC/php/logout.php" class="card logout">Logout</a>
     </div>
 
-    
+    <div class="recentSubmit">
+        <?php if ($result->num_rows === 0): ?>
+            <p>No papers assigned yet.</p>
+        <?php else: ?>
+           
+                <div class="journal">
+                    <div class="journal-body">
+                        <h3><?= htmlspecialchars($row['title']) ?></h3>
+                        <a href="reviewPaper.php?paper_id=<?= $row['paper_id'] ?>">
+                            Review Paper
+                        </a>
+                    </div>
+                </div>
+            
+        <?php endif; ?>
+    </div>
 
 </div>
 
